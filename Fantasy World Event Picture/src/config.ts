64b03124b7776559d41aa0907ef400e856f2849a
longvuu <herryvu.dev@gmail.com>
2025-04-25 20:41:55 +0700

    up

--- Fantasy World Event Picture/src/config.ts
+++ Fantasy World Event Picture/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    token: process.env.TOKEN || '',
+    token: process.env.TOKEN || ' ',
     prefix: 'fw',
     targetChannelId: '1364771923116163093',
     reactions: ['👍', '❤️', '🔥', '🎉', '🥰']
